6e91d38e3f40c0761f923adf1c6f6ee9d673a036
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-09-03 21:50:23 +0600

    responsive

--- JS/news.js
+++ JS/news.js
@@ -2,6 +2,7 @@ const loadCategory = () => {
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
         .then(res => res.json())
         .then(data => displayCategory(data.data.news_category))
+        .catch(error => console.log(error))
 }
 
 const displayCategory = data => {
@@ -21,35 +22,44 @@ const loadNewsList = (id) => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayNewsList(data.data))
+        .catch(error => console.log(error))
 }
 
 const displayNewsList = newsList => {
     // Loader
     toggleLoader(true);
 
+    // category
     const catagory = document.getElementById('catagory');
     catagory.innerHTML = `<h2>${newsList.length} items found for this category</h2>`
+
+    // array sorting
+    // newsList.sort(function () { return newsList.total_view - newsList.total_view });
+    // const sorting = newsList.total_view - newsList.total_view;
+    // console.log(sorting)
 
     const newsListDetail = document.getElementById('news-list');
     newsListDetail.textContent = '';
     newsList.forEach(news => {
-        console.log(news)
+
+
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('flex')
         newsDiv.classList.add('mt-4')
 
-        newsDiv.innerHTML = `
-                    <figure><img class="" src="${news.thumbnail_url}" alt="Album"></figure>
+        newsDiv.innerHTML = `<div class="card lg:card-side bg-base-100 shadow-xl my-4">
+                            <figure><img class="" src="${news.thumbnail_url}" alt="Album"></figure>
                     <div class="card-body">
-                        <h2 class="card-title">${news.title.length > 80 ? news.title.slice(0, 100) + '...' : news.title}</h2>
-                        <p>${news.details.slice(0, 315)}...</p>
+                            <h2 class="card-title">${news.title.length > 80 ? news.title.slice(0, 100) + '...' : news.title}</h2>
+                            <p>${news.details.slice(0, 315)}...</p>
                        
                         <div class="card-actions flex">
-                        <img class="w-1/12 rounded-full" src="${news.author.img}" alt="Album">
-                        <p>${news.author.name ? news.author.name : "No author name found"}</p>
-                        <p><i class="fa-solid fa-eye"></i>${news.total_view ? news.total_view : "No view number found"}</p>
+                            <img class="w-1/12 rounded-full" src="${news.author.img}" alt="Album">
+                            <p>${news.author.name ? news.author.name : "No author name found"}</p>
+                            <p><i class="fa-solid fa-eye"></i>${news.total_view ? news.total_view : "No view number found"}</p>
                             <label onclick="loadNewsDetail('${news._id}')" for="my-modal-6" class="btn modal-button">News Details</label>
                         </div>
+                    </div>
                     </div>
                 
         `
@@ -64,10 +74,11 @@ const loadNewsDetail = (detail) => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayNewsDetail(data.data[0]))
+        .catch(error => console.log(error))
 };
 
 const displayNewsDetail = newsDetail => {
-    console.log(newsDetail)
+
     const modalTitle = document.getElementById('news-title')
     modalTitle.innerText = newsDetail.title;
     const modalDetail = document.getElementById('news-details')
@@ -87,4 +98,4 @@ const toggleLoader = isLoading => {
     }
 }
 
-loadCategory(08);
+loadCategory();

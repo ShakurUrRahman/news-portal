251299243f266b7884ed196447725d3b571ec066
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-09-04 01:28:32 +0600

    final

--- JS/news.js
+++ JS/news.js
@@ -1,3 +1,4 @@
+/* News category Loading */
 const loadCategory = () => {
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
         .then(res => res.json())
@@ -5,50 +6,51 @@ const loadCategory = () => {
         .catch(error => console.log(error))
 }
 
+/* Display category list */
 const displayCategory = data => {
     const categoryContainer = document.getElementById('category-container');
 
     data.forEach(news => {
-
-        const a = document.createElement('a');
-        a.setAttribute('onclick', `loadNewsList(${news.category_id})`)
-        // a.innerHTML = `
-
-        // <li onclick="loadNewsList('${news.category_id}')">${news.category_name}</li>
-        // `;
-        a.innerText = news.category_name;
-        categoryContainer.appendChild(a)
+        const categoryList = document.createElement('a');
+        categoryList.setAttribute('onclick', `loadNewsList(${news.category_id})`)
+        categoryList.innerText = news.category_name;
+        categoryContainer.appendChild(categoryList)
     })
 };
+
+/* Load news list */
 const loadNewsList = (id) => {
     const url = `https://openapi.programming-hero.com/api/news/category/${'0' + id}`
-    console.log(url, id)
+
     fetch(url)
         .then(res => res.json())
         .then(data => displayNewsList(data.data))
         .catch(error => console.log(error))
 }
 
+/* Display news list */
 const displayNewsList = newsList => {
+
     // Loader
     toggleLoader(true);
     console.log(newsList)
-    // category
+
+    // category list number viewer
     const catagory = document.getElementById('catagory');
     catagory.innerHTML = `<h2>${newsList.length} items found for this category</h2>`
     if (newsList.length == 0) {
         catagory.innerText = "No news here";
     }
 
-    const newsListDetail = document.getElementById('news-list');
-    newsListDetail.textContent = '';
-
+    // array sorter
     newsList.sort(function (a, b) {
         return b.total_view - a.total_view
     })
+
+    // display news list
+    const newsListDetail = document.getElementById('news-list');
+    newsListDetail.textContent = '';
     newsList.forEach(news => {
-
-
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('flex')
         newsDiv.classList.add('mt-4')
@@ -67,14 +69,15 @@ const displayNewsList = newsList => {
                         </div>
                     </div>
                     </div>
-                
         `
         newsListDetail.appendChild(newsDiv)
     })
+
     // stop loader
     toggleLoader(false);
 }
 
+/* Load news detail */
 const loadNewsDetail = (detail) => {
     const url = `https://openapi.programming-hero.com/api/news/${detail}`
     fetch(url)
@@ -83,17 +86,16 @@ const loadNewsDetail = (detail) => {
         .catch(error => console.log(error))
 };
 
+/* Display news detail */
 const displayNewsDetail = newsDetail => {
-
     const modalTitle = document.getElementById('news-title')
     modalTitle.innerText = newsDetail.title;
     const modalDetail = document.getElementById('news-details')
     modalDetail.innerHTML = `<img src="${newsDetail.image_url}"/>
     <p>${newsDetail.details}</p>`
-
-
 }
 
+/* Loader */
 const toggleLoader = isLoading => {
     const loaderSection = document.getElementById('loader');
     if (isLoading) {
@@ -103,5 +105,7 @@ const toggleLoader = isLoading => {
         loaderSection.classList.add('hidden')
     }
 }
+
+/* Default function */
 loadNewsList(08);
 loadCategory();
